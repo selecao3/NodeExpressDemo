5ebd0a18018324a11ffb4d130ba5c9f75a71e702
kazuha <mami.tomoe4@gmail.com>
2019-02-16 02:24:57 +0900

    error handring was finished in index.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -10,60 +10,39 @@ import '../css/form.css';
 import '../css/header.css';
 import '../css/paging.css';
 
-$(function() {
-  $('#submit').on('click', function() {
+$(function () {
+  $('#triggerButton').on('click', function () {
     let errorinfo;
-    // const targetInput = $('input.require').map(function() {
-    //   return $(this).val();
-    // });
-    $('input.require').map(function() {
+    let flag = true;
+    $('input.require').each(function () {
       if ($(this).val() == '' || $(this).val().match(/[^\s\t]/) == false) {
         errorinfo = $(this).next('span.error-info');
         errorinfo.css('display', 'block');
+        flag = false;
       } else {
         errorinfo = $(this).next('span.error-info');
         errorinfo.css('display', 'none');
       }
     });
-    const targetSelect = $('select.require');
-    // const valueByInput = targetInput.val();
-    // const nameBySelect = targetSelect.attr('name');
-    // console.log(targetInput[0].html());
-    // console.log(targetSelect[0].html());
-    // const errorinfo = targetInput.next('span.error-info');
-    // if (value == '') {
-    //   error = true;
-    // } else if (!value.match(/[^\s\t]/)) {
-    //   error = true;
-    // }
-    // if (error) {
-    //   errorinfo.css('display', 'block');
-    // } else {
-    //   errorinfo.css('display', 'none');
-    // }
-    return false;
+    $('select.require').each(function () {
+      const nameBySelect = $(this).attr('name');
+      const target = $(`select[name=\"${nameBySelect}\"] option:selected`);
+      if (target.val() == '' || target.val().match(/[^\s\t]/) == false) {
+        errorinfo = $(this).nextAll('span.error-info');
+        errorinfo.css('display', 'block');
+        flag = false;
+      } else {
+        errorinfo = $(this).nextAll('span.error-info');
+        errorinfo.css('display', 'none');
+      }
+    });
+    if (flag) {
+      $('#userData').submit();
+    }
   });
-
-  // function errorMassageSelect() {
-  //   let error;
-  //   const target = $('select.require');
-  //   const name = target.attr('name');
-  //   const value = $(`select[name=\"${name}\"] option:selected`).val();
-  //   const errorinfo = target.nextAll('span.error-info');
-  //   if (value == '') {
-  //     error = true;
-  //   } else if (!value.match(/[^\s\t]/)) {
-  //     error = true;
-  //   }
-  //   if (error) {
-  //     errorinfo.css('display', 'block');
-  //   } else {
-  //     errorinfo.css('display', 'none');
-  //   }
-  // };
 });
 //$(function(){});でjqueryを使うことができる
-$(function() {
+$(function () {
   $('.date-input').flatpickr({
     locale: 'ja',
     enableTime: true,
@@ -72,18 +51,21 @@ $(function() {
   });
 });
 
-$(function() {
-  $('.date-search').flatpickr({locale: 'ja', dateFormat: 'Y年m月d日'});
+$(function () {
+  $('.date-search').flatpickr({
+    locale: 'ja',
+    dateFormat: 'Y年m月d日'
+  });
 });
 
-$(function() {
+$(function () {
   $('select').niceSelect();
 });
 
-$(function() {
+$(function () {
   const targetArea = $('#checkFinFalseArea');
   const str = targetArea.html();
-  $('#checkFin').change(function() {
+  $('#checkFin').change(function () {
     const prop = $(this).prop('checked');
     if (prop) {
       targetArea.html('');
