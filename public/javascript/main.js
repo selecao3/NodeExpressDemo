63bdab668a97de61cfaad22d2bf09e3ca548e89c
kazuha <mami.tomoe4@gmail.com>
2019-02-18 16:24:52 +0900

    任意のスタッフの追加

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -10,11 +10,12 @@ import '../css/form.css';
 import '../css/header.css';
 import '../css/paging.css';
 
-$(function () {
-  $('#triggerButton').on('click', function () {
+//記入チェックの関数
+$(function() {
+  $('#triggerButton').on('click', function() {
     let errorinfo;
     let flag = true;
-    $('input.require').each(function () {
+    $('input.require').each(function() {
       if ($(this).val() == '' || $(this).val().match(/[^\s\t]/) == false) {
         errorinfo = $(this).next('span.error-info');
         errorinfo.css('display', 'block');
@@ -24,7 +25,7 @@ $(function () {
         errorinfo.css('display', 'none');
       }
     });
-    $('select.require').each(function () {
+    $('select.require').each(function() {
       const nameBySelect = $(this).attr('name');
       const target = $(`select[name=\"${nameBySelect}\"] option:selected`);
       if (target.val() == '' || target.val().match(/[^\s\t]/) == false) {
@@ -41,8 +42,30 @@ $(function () {
     }
   });
 });
+
+$(function() {
+  $('select[name=\"stuff\"]').change(function() {
+    const otherStuff = $('input.otherStuff');
+    const target = $('select[name=\"stuff\"] option:selected');
+    console.log('click!');
+    console.log(target.val());
+    if (target.val() == 'その他') {
+      console.log('if!');
+      otherStuff.css('display', 'block');
+      const otherStuffName = otherStuff.val();
+      otherStuff.on('input', function() {
+        target.val(otherStuffName);
+        target.html(otherStuffName);
+      })
+    } else {
+      console.log('else!');
+      otherStuff.css('display', 'none');
+    }
+  })
+})
+
 //$(function(){});でjqueryを使うことができる
-$(function () {
+$(function() {
   $('.date-input').flatpickr({
     locale: 'ja',
     enableTime: true,
@@ -51,21 +74,18 @@ $(function () {
   });
 });
 
-$(function () {
-  $('.date-search').flatpickr({
-    locale: 'ja',
-    dateFormat: 'Y年m月d日'
-  });
+$(function() {
+  $('.date-search').flatpickr({locale: 'ja', dateFormat: 'Y年m月d日'});
 });
 
-$(function () {
+$(function() {
   $('select').niceSelect();
 });
 
-$(function () {
+$(function() {
   const targetArea = $('#checkFinFalseArea');
   const str = targetArea.html();
-  $('#checkFin').change(function () {
+  $('#checkFin').change(function() {
     const prop = $(this).prop('checked');
     if (prop) {
       targetArea.html('');
