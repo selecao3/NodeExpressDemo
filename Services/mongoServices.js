9f304188766d11f399c74b3cd62f81632120cfc9
kazuha <mami.tomoe4@gmail.com>
2019-02-27 21:08:11 +0900

    csv実装完了

--- Services/mongoServices.js
+++ Services/mongoServices.js
@@ -2,41 +2,63 @@ const mc = require('../Repository/MongoConnect');
 
 // findBy***シリーズ
 // ***の部分を探して結果を返す
-exports.findByQuestions = function(itemName, page) {
-  const query = mc.formedModel.find({questions: itemName}).sort({date: -1});
-  const result = mc.formedModel.paginate(query, {page: page, limit: 5});
+exports.findByQuestions = function (itemName, page) {
+  const query = mc.formedModel.find({
+    questions: itemName
+  }).sort({
+    date: -1
+  });
+  const result = mc.formedModel.paginate(query, {
+    page: page,
+    limit: 5
+  });
   return result;
 };
 
-exports.findById = function(id) {
-  const result = mc.formedModel.findOne({_id: id}).exec();
+exports.findById = function (id) {
+  const result = mc.formedModel.findOne({
+    _id: id
+  }).exec();
   return result;
 };
 
-exports.findByTitleForLatestDates = function(title) {
+exports.findByTitleForLatestDates = function (title) {
   const result = [];
   let tmp;
   title.forEach(ele => {
-    tmp = mc.formedModel.findOne({questions: ele}).sort({date: -1}).exec();
+    tmp = mc.formedModel.findOne({
+      questions: ele
+    }).sort({
+      date: -1
+    }).exec();
     result.push(tmp);
   });
   return result;
 };
 // findBy***シリーズ fin
 
-exports.FinCheck2True = function(itemID) {
+exports.FinCheck2True = function (itemID) {
   console.log(itemID);
   // idではなく、_id
-  mc.formedModel.updateOne(
-      {_id: itemID}, {$set: {checkFin: '1'}}, {upsert: false}, function(err) {
-        if (err) {
-          console.log(err);
-        }
-      });
+  mc.formedModel.updateOne({
+    _id: itemID
+  }, {
+    $set: {
+      checkFin: '1'
+    }
+  }, {
+    upsert: false
+  }, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 };
 
-exports.deleteByID = function(itemID) {
-  mc.formedModel.deleteOne({_id: itemID}, function(err) {
+exports.deleteByID = function (itemID) {
+  mc.formedModel.deleteOne({
+    _id: itemID
+  }, function (err) {
     if (err) {
       console.log('error');
       return false;
@@ -45,33 +67,35 @@ exports.deleteByID = function(itemID) {
   return true;
 };
 
-exports.updateOneDataByBody = function(data, id) {
+exports.updateOneDataByBody = function (data, id) {
   // idではなく、_id
   console.log(data.questionsCon);
-  mc.formedModel.updateOne(
-      {_id: id}, {
-        $set: {
-          stuff: data.stuff,
-          ident: data.ident,
-          questioner: data.questioner,
-          questionersNumber: data.questionersNumber,
-          questionersAddress: data.questionersAddress,
-          questionersID: data.questionersID,
-          questions: data.questions,
-          questionsCon: data.questionsCon,
-          specialText: data.specialText
-        }
-      },
-      {upsert: false}, function(err) {
-        if (err) {
-          console.log(err);
-          return false;
-        }
-      });
+  mc.formedModel.updateOne({
+    _id: id
+  }, {
+    $set: {
+      stuff: data.stuff,
+      ident: data.ident,
+      questioner: data.questioner,
+      questionersNumber: data.questionersNumber,
+      questionersAddress: data.questionersAddress,
+      questionersID: data.questionersID,
+      questions: data.questions,
+      questionsCon: data.questionsCon,
+      specialText: data.specialText
+    }
+  }, {
+    upsert: false
+  }, function (err) {
+    if (err) {
+      console.log(err);
+      return false;
+    }
+  });
   return true;
 };
 
-exports.savesForQuestionerData = function(body) {
+exports.savesForQuestionerData = function (body) {
   // stuffでその他を選択してpostした場合、['その他','（任意の値）']で渡されるので0番目をここで弾く
 
   let stuff;
@@ -97,7 +121,8 @@ exports.savesForQuestionerData = function(body) {
   return model.save();
 };
 
-exports.searchByBody = function(body, page) {
+// 検索画面で入力されたデータを元に該当するデータをページ数と共に返す
+exports.searchedDataWithPage = function (body, page) {
   const searchTarget = {
     // date: body.date,
     date: new RegExp(body.date),
@@ -108,13 +133,43 @@ exports.searchByBody = function(body, page) {
     questionersID: body.questionersID,
     checkFin: body.checkFin
   };
+  // 空欄のvalueをもつ項目を削除
   for (key in searchTarget) {
     if (searchTarget[key] == '') {
       delete searchTarget[key];
     }
   }
   console.log(searchTarget);
-  const query = mc.formedModel.find(searchTarget).sort({date: -1});
-  const res = mc.formedModel.paginate(query, {page: page, limit: 5});
+  const query = mc.formedModel.find(searchTarget).sort({
+    date: -1
+  });
+  const res = mc.formedModel.paginate(query, {
+    page: page,
+    limit: 5
+  });
   return res;
+};
+
+exports.searchedDataExportCSV = function (body) {
+  const searchTarget = {
+    // date: body.date,
+    date: new RegExp(body.date),
+    stuff: body.stuff,
+    ident: body.ident,
+    questioner: new RegExp(body.questioner),
+    questionersNumber: body.questionersNumber,
+    questionersID: body.questionersID,
+    checkFin: body.checkFin
+  };
+  // 空欄のvalueをもつ項目を削除
+  for (key in searchTarget) {
+    if (searchTarget[key] == '') {
+      delete searchTarget[key];
+    }
+  }
+  console.log(searchTarget);
+  const result = mc.formedModel.find(searchTarget).sort({
+    date: -1
+  });
+  return result;
 };
--- routes/post.js
+++ routes/post.js
@@ -3,30 +3,33 @@ const ms = require('../Services/mongoServices');
 
 const router = express.Router();
 
-router.post('/postData', function(req, res, next) {
+router.post('/postData', function (req, res, next) {
   const promise = ms.savesForQuestionerData(req.body);
   promise.then(
-      function() {
-        res.redirect(302, '/');
-      },
-      function() {
-        res.redirect(503, '/');
-      });
+    function () {
+      res.redirect(302, '/');
+    },
+    function () {
+      res.redirect(503, '/');
+    });
 });
 
-router.post('/search', function(req, res, next) {
-  res.cookie('searchRes', req.body, {path: '/search'});
+router.post('/search', function (req, res, next) {
+  res.cookie('searchRes', req.body, {
+    path: '/search'
+  });
   res.redirect(301, '/search/result');
 });
 
-router.post('/exportCSV', function(req, res, next) {
-  res.cookie('csvRes', req.body, {path: '/exportCSV'});
-  console.log(csvRes);
-  res.redirect(301, '/search/result');
+router.post('/exportCSV', function (req, res, next) {
+  res.cookie('csvRes', req.body, {
+    path: '/exportCSV'
+  });
+  res.redirect(301, '/exportCSV/result');
 });
 
 
-router.post('/updateData/:itemName/:itemID', function(req, res, next) {
+router.post('/updateData/:itemName/:itemID', function (req, res, next) {
   const itemID = req.params.itemID;
   const itemName = req.params.itemName;
   const flag = ms.updateOneDataByBody(req.body, itemID);
